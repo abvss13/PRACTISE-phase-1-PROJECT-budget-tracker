ff655547b36487e2d60edac6f9b9a848bef31bda
Your Name <abdullahi.abass@student.moringschool.com>
2023-07-05 21:08:46 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const balanceEl = document.getElementById("balance");
+const incomeEl = document.getElementById("income");
+const expensesEl = document.getElementById("expenses");
+const transactionsListEl = document.getElementById("transactions-list");
+const addTransactionFormEl = document.getElementById("addtransaction");
+const categoryInputEl = document.getElementById("category");
+const amountInputEl = document.getElementById("amount");
